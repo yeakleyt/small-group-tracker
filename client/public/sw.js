8d7bab6b63a7839b3f0be8695daf77fa94592762
yeakleyt <yeakleyt@gmail.com>
2026-04-15 16:25:08 -0500

    updated version

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,5 +1,5 @@
 // Small Group Manager — Service Worker
-const CACHE_NAME = "sgm-v3";
+const CACHE_NAME = "sgm-v4";
 
 // On install — cache the app shell
 self.addEventListener("install", event => {
